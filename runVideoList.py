6676bbaa228efb9026f211a0e22d151e02c949c7
mcfoster <cfoster@mcfse.com>
2020-04-30 16:52:13 -0400

    1) Changed the method for creating a background method so that we have a frame_sum which is the sum of the frames to be averaged, and a frame_keep which is a list of the frames to be averaged. 2) Added running average code for the running video frames to update the background mask in case new debris enters into the field of view. 3) Added some timing print outs. 4) Added printout and results file output for floating debris if detected.

--- runVideoList.py
+++ runVideoList.py
@@ -12,11 +12,11 @@ import tracker
 import cv2
 import csv
 import sys
+import time
 from os import path
 from pathlib import Path
 
 results_file = open("results.txt", "w")
-
 def runSingleVideo(videoFileName, max_blob = 2800, flip = False, good_count = 200):
     #print(videoFileName)
     global passed_count, failed_count
@@ -38,15 +38,17 @@ def runSingleVideo(videoFileName, max_blob = 2800, flip = False, good_count = 20
     h = int(cVideo.get(cv2.CAP_PROP_FRAME_HEIGHT))
     w = int(cVideo.get(cv2.CAP_PROP_FRAME_WIDTH))
     print("dimensions: " + str(h) + ", " + str(w))
-    frameStruct = {'frameStore': np.zeros((h, w, 45))}
 
-    for i in range(45): # was 45
+    # mcf: Keep sum model for future trash compensation
+    frame_sum = np.zeros([h, w])
+    frame_keep = []
+    avg_count = 25  # was 45
+    for i in range(avg_count):
         _, im = cVideo.read()
-        frameStruct['frameStore'][:, :, i] = im[:, :, 0]
+        frame_sum += im[:, :, 0]
+        frame_keep.append(im[:, :, 0])
         cCount.append(np.uint8(0)) # no count for these frames I hope
-
-        ######### run model
-    backgroundModel = np.mean(frameStruct['frameStore'][:, :, :], axis=2)
+    backgroundModel = frame_sum / avg_count
 
     bA = myBlobAnalyzerSide()
 
@@ -57,7 +59,8 @@ def runSingleVideo(videoFileName, max_blob = 2800, flip = False, good_count = 20
     gray_cutoff = 150
     frame = (255. - backgroundModel).astype('uint8')
     if flip:
-        frame = np.flip(np.flip(frame, 0), 1)
+        np.flip(frame)
+
 
     frame[frame < gray_cutoff] = 0
     frame[frame >= 55] = 1
@@ -72,7 +75,7 @@ def runSingleVideo(videoFileName, max_blob = 2800, flip = False, good_count = 20
 
     bA.sideViewSizeAdjust = 50  # 75
 
-    estVelStart = np.array([[0, 9]], 'double')  # [0,50].*(120/FPS).*(fliplr(size(frame)./[480,720]));
+    estVelStart = np.array([[0, 9]], 'double')
 
     maxAssign = 115  # 65#110#120#65
     # 1280.0 3400.0 2800.0
@@ -91,26 +94,40 @@ def runSingleVideo(videoFileName, max_blob = 2800, flip = False, good_count = 20
 
     isValid, cFrame = cVideo.read()
     iFrame = 0
+    sum_ms = 0.0
+    max_ms = 0.0
     while isValid:
+        start = time.time()
         # _,cFrame = cVideo.read()
         iFrame = iFrame + 1
 
-        # frame = abs(backgroundModel - cFrame[:,:,0]).astype('uint8')
+        # mcf: background mask
+        frame_sum -= frame_keep[0]  # subtract the oldest
+        frame_sum += cFrame[:, :, 0]  #  add the new frame
+        frame_keep.pop(0)  # Remove oldest frame from the bottom of the list
+        frame_keep.append(cFrame[:, :, 0])  # Put newest frame on top of the list.
+        bk_frame = (255. - (frame_sum / avg_count)).astype('uint8')
+        bk_frame[bk_frame < 100] = 0
+        bk_frame[bk_frame >= 1] = 1
+
         frame = abs(backgroundModel - cFrame[:, :, 0]).astype('uint8')
         if flipHer:
-            frame = np.flip(np.flip(frame, 0), 1)
+            frame = np.flip(frame)
+            # mcf background mask
+            bk_frame = np.flip(bk_frame)
+
+        kernel = np.ones((4, 4), np.uint8)
+        bA.bkmask = cv2.dilate(bk_frame, kernel, iterations=1)
 
         frame[frame < gray_cutoff] = 0
         frame[frame >= 55] = 1
 
-        tracksNew, nextId = tracker.step(frame, bA, tracks, maxAssign, curId, estVelStart, numFrames, False, maxBlob,
-                                         minBlob)
-        cCount.append(np.uint8(nextId-1))
+        tracksNew, nextId = tracker.step(frame, bA, tracks, maxAssign, curId, estVelStart,
+                                         numFrames, False, maxBlob, minBlob)
+        cCount.append(np.uint8(nextId - 1))
 
         tracks = tracksNew.copy();
-
         numTracks[i] = len(tracksNew)
-
         if (nextId - curId) != 0:
             print("pill " + str(curId) + " found at " + str(iFrame))
             countVec[i] = curId
@@ -121,18 +138,27 @@ def runSingleVideo(videoFileName, max_blob = 2800, flip = False, good_count = 20
         # break
         isValid, cFrame = cVideo.read()
 
+        ms = (time.time() - start) * 1000
+        sum_ms += ms
+        if max_ms < ms:
+            max_ms = ms
+        print('frame time:{:.4}ms'.format(ms))
+
     # print this to log file
     print(f'min blob: {bA.minBlobArea}')
     print(f'max blob: {bA.maxBlobSize}')
     print('Flip: ', flipHer)
     print("count: " + str(curId - 1))
+    if iFrame > 0:
+        ave_ms = sum_ms / iFrame
+        print('Avg frame time : {:.4}ms'.format(ave_ms))
+        print('Max frame time : {:.4}ms'.format(max_ms))
 
     sys.stdout.flush()
     sys.stdout.close()
 
     # print set to console
     sys.stdout = stdout_fileno
-
 
     print(f'min blob: {bA.minBlobArea}')
     print(f'max blob: {bA.maxBlobSize}')
@@ -152,6 +178,9 @@ def runSingleVideo(videoFileName, max_blob = 2800, flip = False, good_count = 20
         results_file.write("Failed expected {} counted {}\n".format(good_count, (curId-1)))
         print("Failed expected {} counted {}\n".format(good_count, (curId-1)))
         failed_count += 1
+    if bA.floating_debris:
+        results_file.write("Possible miscount, moving debris detected.\n")
+        print("Possible miscount, moving debris detected.")
 
 if __name__ == "__main__":
     global passed_count, failed_count
@@ -193,10 +222,11 @@ if __name__ == "__main__":
     results_file.write("Videos run = {}\n".format(passed_count + failed_count))
     results_file.write("Total passed = {}\n".format(passed_count))
     results_file.write("Total failed = {}\n".format(failed_count))
+
     # show results on console
     print("***** COMPLETE *****")
-    print("Videos run = {}".format(passed_count+failed_count) )
-    print("Total passed = {}".format(passed_count) )
-    print("Total failed = {}".format(failed_count) )
+    print("Videos run = {}".format(passed_count+failed_count))
+    print("Total passed = {}".format(passed_count))
+    print("Total failed = {}".format(failed_count))
 
     results_file.close()
